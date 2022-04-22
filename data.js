d4d23cfc7c2867cce8bdc5eda86922a01f3a1597
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-21 21:30:35 -0400

    Adding data

--- data.js
+++ data.js
@@ -1,26 +1,66 @@
 const monthlyData = [
 {
+  "name": "Oyster Mushroom",
+  "img_url": "https://en.wikipedia.org/wiki/Pleurotus#/media/File:Oyster_mushoom_fells.jpg",
   "id": 1,
-  "month": "January",
+  "month": ["December", "January"],
   "sign": "Capricorn",
   "messages": ["hello Janurary Capricorn ", "Hi January"]
 },
 {
+  "name": "Fly Agaric",
+  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Amanita_muscaria_(fly_agaric).jpg",
+  "id": 2,
+  "month": ["January", "February"]
+  "sign": "Aquarius",
+  "messages": ["hello Janurary Aquarius ", "Jan Aquarius"]
+},
+{
+  "name": "Fly Agaric",
+  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Amanita_muscaria_(fly_agaric).JPG",
   "id": 2,
   "month": "February",
   "sign": "Capricorn",
   "messages": ["hello February Capricorn", "Hi February"]
 },
 {
+  "name": "Indigo Milk Cap",
+  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Lactarius_indigo_48568.jpg",
   "id": 3,
   "month": "March",
   "sign": "Pisces",
   "messages": ["hello March Peices", "Hi March"]
 },
 {
+  "name": "Indigo Milk Cap",
+  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Lactarius_indigo_48568.jpg",
   "id": 4,
   "month": "April",
   "sign": "Aries",
+  "messages": ["hello April Aries", "Hi April"]
+},
+{
+  "name": "Indigo Milk Cap",
+  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Lactarius_indigo_48568.jpg",
+  "id": 5,
+  "month": "May",
+  "sign": "Aries",
+  "messages": ["hello April Aries", "Hi April"]
+},
+{
+  "name": "Indigo Milk Cap",
+  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Lactarius_indigo_48568.jpg",
+  "id": 6,
+  "month": "June",
+  "sign": "",
+  "messages": ["hello April Aries", "Hi April"]
+},
+{
+  "name": "Indigo Milk Cap",
+  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Lactarius_indigo_48568.jpg",
+  "id": 7,
+  "month": "July",
+  "sign": "",
   "messages": ["hello April Aries", "Hi April"]
 }
 
