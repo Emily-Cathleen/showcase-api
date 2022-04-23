5c167760dfd69d2b7a63c40a2eff71148b7ff6c3
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-23 17:43:17 -0400

    updated my API to include all months of the year with unique mushrooms, fixed months so they include 2 months with 1 id.

--- data.js
+++ data.js
@@ -19,49 +19,81 @@ const monthlyData = [
   "name": "Psilocybin Mushrooms",
   "img_url": "https://upload.wikimedia.org/wikipedia/commons/4/4a/Psilocybe_semilanceata_6514.jpg",
   "id": 3,
-  "month": "February",
-  "sign": "Capricorn",
+  "month": ["February", "March"],
+  "sign": "Pisces",
   "messages": ["hello February Capricorn", "Hi February"]
 },
 {
   "name": "Indigo Milk Cap",
   "img_url": "https://upload.wikimedia.org/wikipedia/commons/6/6e/Lactarius_indigo_48568.jpg",
   "id": 4,
-  "month": "March",
+  "month": ["March", "April"],
   "sign": "Pisces",
   "messages": ["hello March Peices", "Hi March"]
 },
 {
-  "name": "Indigo Milk Cap",
-  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Lactarius_indigo_48568.jpg",
-  "id": 4,
-  "month": "April",
-  "sign": "Aries",
-  "messages": ["hello April Aries", "Hi April"]
-},
-{
-  "name": "Indigo Milk Cap",
-  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Lactarius_indigo_48568.jpg",
+  "name": "Lions Mane",
+  "img_url": "https://upload.wikimedia.org/wikipedia/commons/0/00/Igelstachelbart_Nov_06.jpg",
   "id": 5,
-  "month": "May",
-  "sign": "Aries",
+  "month": ["April", "May"],
+  "sign": "Taurus",
   "messages": ["hello April Aries", "Hi April"]
 },
 {
-  "name": "Indigo Milk Cap",
-  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Lactarius_indigo_48568.jpg",
+  "name": "Agaricus bisporus: Portobello",
+  "img_url": "https://upload.wikimedia.org/wikipedia/commons/4/4f/Giant_mushroom_cross-section.jpg",
   "id": 6,
-  "month": "June",
-  "sign": "",
-  "messages": ["hello April Aries", "Hi April"]
+  "month": ["May", "June"],
+  "sign": "Gemini",
+  "messages": ["Hello Gemini"]
 },
 {
-  "name": "Indigo Milk Cap",
-  "img_url": "https://en.wikipedia.org/wiki/Mushroom#/media/File:Lactarius_indigo_48568.jpg",
+  "name": "Enoki Mushroom",
+  "img_url": "https://upload.wikimedia.org/wikipedia/commons/a/aa/Flammulina_velutipes.JPG",
   "id": 7,
-  "month": "July",
-  "sign": "",
-  "messages": ["hello April Aries", "Hi April"]
+  "month": ["June", "July"],
+  "sign": "Cancer",
+  "messages": ["Hello Cancer"]
+},
+{
+  "name": "Chanterelle Mushroom",
+  "img_url": "https://upload.wikimedia.org/wikipedia/commons/0/02/2007-07-14_Cantharellus_cibarius_Detail.jpg",
+  "id": 8,
+  "month": ["July", "August"],
+  "sign": "Leo",
+  "messages": ["Hello Leo"]
+},
+{
+  "name": "Porcini Mushroom",
+  "img_url": "https://upload.wikimedia.org/wikipedia/commons/5/5b/Boletus_edulis_var._grandedulis_27911.jpg",
+  "id": 9,
+  "month": ["August", "September"],
+  "sign": "Virgo",
+  "messages": ["hello Virgo"]
+},
+{
+  "name": "Wood Hedgehog Mushroom",
+  "img_url": "https://upload.wikimedia.org/wikipedia/commons/b/b9/Hedgehog_fungi2.jpg",
+  "id": 10,
+  "month": ["September", "October"],
+  "sign": "Libra",
+  "messages": ["Hello Libra"]
+},
+{
+  "name": "Morels",
+  "img_url": "https://upload.wikimedia.org/wikipedia/commons/8/82/Morchella_conica_1_beentree.jpg",
+  "id": 11,
+  "month": ["October", "November"],
+  "sign": "Scorpio",
+  "messages": ["hello Scorpio"]
+},
+{
+  "name": "Black Trumpet Mushroom",
+  "img_url": "https://upload.wikimedia.org/wikipedia/commons/c/ca/Craterellus_cornucopioides_JPG1.jpg",
+  "id": 12,
+  "month": ["November", "December"],
+  "sign": "Sagittarius",
+  "messages": ["hello Sagittarius"]
 }
 
 ]
